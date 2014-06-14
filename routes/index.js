4ffbd397082ede78c148ddf72332aac4f846bf69
raelon <steven.godbold@gmail.com>
2014-06-14 12:51:22 -0700

    Finished starting html template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
+});
+
+/* GET about page. */
+router.get('/about', function(req, res) {
+    res.render('about');
+});
+
+/* GET about page. */
+router.get('/contact', function(req, res) {
+    res.render('contact');
 });
 
 module.exports = router;
